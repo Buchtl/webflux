3be2ccfd4599d74c81f49d976bbb4f45117d5ad5
Chris <schauer@inf-schauer.de>
2024-08-16 15:34:24 +0200

    init spring

---
+++ src/test/kotlin/de/inf_schauer/webflux/WebfluxApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.inf_schauer.webflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
